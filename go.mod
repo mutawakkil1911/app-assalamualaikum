2e90ee9b8d902a0ecc5d3979938496989ef46000
mutawakkil1911 <mutawakkilalallahrodriguez25@gmail.com>
2021-06-24 17:36:26 +0700

    Membuat aplikasi assalamualaikum

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mutawakkil1911/app-assalamualaikum
+
+go 1.16
+
+require github.com/mutawakkil1911/assalamualaikum v1.5.0 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	assalamualaikum "github.com/mutawakkil1911/assalamualaikum"
+)
+
+func main() {
+	fmt.Println("Ucapkan salam...")
+	fmt.Println(assalamualaikum.Assalamualaikum())
+}
